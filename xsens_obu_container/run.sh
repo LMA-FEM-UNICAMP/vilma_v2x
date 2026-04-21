a5987fa01dd6217cd5b0ed5fb52707cfb1ac15bb
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2026-04-21 17:23:51 -0300

    Updating docker run to use all /dev devices

--- xsens_obu_container/run.sh
+++ xsens_obu_container/run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-docker run -it --rm \
-  --net=host \
-  --ipc=host \
-  --cap-add=NET_ADMIN \
-  --cap-add=NET_RAW \
-  --device=/dev/ttyUSB0:/dev/ttyUSB0 \
-  xsens_obu_container:ARM_V1
+# Let all /dev to container once xsens package can find the IMU in any port and the OBU sometimes mount it in random ports.
+
+docker run -it --rm --net=host   --ipc=host   --cap-add=NET_ADMIN   --cap-add=NET_RAW  --privileged --device=/dev:/dev  --name xsens_obu_container xsens_obu_container:ARM_V2
+   
+  
